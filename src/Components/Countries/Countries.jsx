8db3beb67e7008b18b5576967ef989139e39cc06
jahidsharuar2021-prog <jahidsharuar2021@gmail.com>
2026-03-31 15:59:53 +0600

    Api theke data load

--- src/Components/Countries/Countries.jsx
+++ src/Components/Countries/Countries.jsx
@@ -12,7 +12,7 @@ const Countries = ({ countriesPromise }) => {
   return (
     <div>
       <h1>In the countries:{countries.length}</h1>
-      {countries.map(country => <Country country={country}></Country>
+      {countries.map(country => <Country key={country.cca3.cca3} country={country}></Country>
       )}
     </div>
   );
--- src/Components/Country/Country.jsx
+++ src/Components/Country/Country.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
 const Country = ({ country }) => {
-  console.log(country.name.common);
+  console.log(country.capital.capital);
   return (
     <div>
+      <img src={country.flags.flags.png} alt="country flag " />
       <h3>Name:{country.name.common}</h3>
+      <h5>Capital:{country.capital.capital}</h5>
+      <p>Population:{country.population.population}</p>
+      <br />
     </div>
   );
 };
